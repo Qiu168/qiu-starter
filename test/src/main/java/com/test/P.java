ccf06539939034c43ab10c6aa707721ff3160bec
qiu <1462909477@qq.com>
2023-11-11 23:45:51 +0800

    refactor

--- test/src/main/java/com/test/P.java
+++ test/src/main/java/com/test/P.java
@@ -1,6 +1,5 @@
 package com.test;
 
-import com.cat.json.GeneratePrint;
 import com.cat.json.JSONIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,6 +9,5 @@ import lombok.Data;
 public class P{
     @JSONIgnore
     String name;
-    @GeneratePrint
     String password;
 }
