6f14aa1b8ba30ec9d541a3edd475594cb6785ba4
qiu <1462909477@qq.com>
2023-11-11 20:04:48 +0800

    json

---
+++ cat-spring-boot-autoconfigure/src/main/java/com/cat/json/JSONIgnore.java
@@ -0,0 +1,25 @@
+package com.cat.json;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 模仿{@link com.fasterxml.jackson.annotation.JsonIgnore JsonIgnore}
+ * 此处是fastjson的JSONIgnore
+ * 区别就是fastjson喜欢用JSON
+ * 不影响反序列化，maybe
+ * @author _qiu
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@JsonIgnore
+@JSONField(serialize = false)
+public @interface JSONIgnore {
+}
