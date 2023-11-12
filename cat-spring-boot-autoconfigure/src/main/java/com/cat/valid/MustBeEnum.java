c74365b366a3d1536a3a7649b3739e1dedfa3a88
qiu <1462909477@qq.com>
2023-11-12 20:49:29 +0800

    MustBeEnum

--- cat-spring-boot-autoconfigure/src/main/java/com/cat/valid/MustBeEnum.java
+++ cat-spring-boot-autoconfigure/src/main/java/com/cat/valid/MustBeEnum.java
@@ -6,9 +6,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ *
+ * 这是一个编译时注解，用来放到接口上，继承此接口的实现类必须是ENUM <p>
+ * 很遗憾这似乎是不能晇包的，因为他是一个一个包编译的。<p></p>
+ * 因为编译器不会从头到尾编译全部内容，似乎只会编译修改的。如果执意执行：(报错后再次执行)该注解无法检测 <p></p>
  * todo
- * 这是一个编译时注解，用来放到接口上，继承此接口的实现类必须是ENUM
- * @author 14629
+ *  我已试过将这个注解抽取出来单独放一个模块，但是A包定义的注解，B包实现无法检查
+ * @author _qiu
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
--- cat-spring-boot-autoconfigure/src/main/java/com/cat/valid/MustBeEnumProcessor.java
+++ cat-spring-boot-autoconfigure/src/main/java/com/cat/valid/MustBeEnumProcessor.java
@@ -1,6 +1,6 @@
 package com.cat.valid;
 
-//import com.google.auto.service.AutoService;
+import com.google.auto.service.AutoService;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
@@ -9,18 +9,21 @@ import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 /**
- * todo:求大佬带带不会写啊，为什么就是不成功呢？
- * @author 14629
+ * @see MustBeEnum
+ * @author _qiu
  */
-//@AutoService(Processor.class)
+@AutoService(Processor.class)
 @SupportedAnnotationTypes("com.cat.valid.MustBeEnum")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class MustBeEnumProcessor extends AbstractProcessor {
 
     private Elements elementUtils;
+    private final List<Element> interfaceList=new ArrayList<>();
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
@@ -32,25 +35,36 @@ public class MustBeEnumProcessor extends AbstractProcessor {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (Element element : roundEnv.getElementsAnnotatedWith(MustBeEnum.class)) {
             if (element.getKind() == ElementKind.INTERFACE) {
-                checkImplementations((TypeElement) element);
+                interfaceList.add(element);
             } else {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
-                        "@MustBeEnum must be applied to an interface.");
+                        "@MustBeEnum must be applied to an interface.",element);
             }
         }
+        checkImplementations(roundEnv);
         return true;
     }
 
-    private void checkImplementations(TypeElement interfaceElement) {
-        for (Element element : elementUtils.getAllMembers(interfaceElement)) {
+    private void checkImplementations(RoundEnvironment roundEnv) {
+
+        if(interfaceList.size()==0){
+            return;
+        }
+
+        for (Element element : roundEnv.getRootElements()) {
             if (element.getKind() == ElementKind.CLASS) {
-                TypeElement classElement = (TypeElement) element;
-                // 检查类是否是枚举类型
-                if (classElement.getKind() != ElementKind.ENUM) {
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
-                            "Classes implementing @MustBeEnum annotated interface must be enums.",
-                            classElement);
+                for (Element inter :interfaceList){
+                    if(processingEnv.getTypeUtils().isSubtype(element.asType(),inter.asType())){
+                        TypeElement classElement = (TypeElement) element;
+                        if (classElement.getKind() != ElementKind.ENUM) {
+                            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+                                    "Classes implementing @MustBeEnum annotated interface must be enums.",
+                                    classElement);
+                        }
+                        break;
+                    }
                 }
+
             }
         }
     }
