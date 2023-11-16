ec20b475472b952796997dc06dc1471d19889c36
qiu <1462909477@qq.com>
2023-11-16 13:01:20 +0800

    feat：新增缓存组件

--- cat-spring-boot-autoconfigure/src/main/java/com/cat/redis/RedisTemplateConfiguration.java
+++ cat-spring-boot-autoconfigure/src/main/java/com/cat/redis/RedisTemplateConfiguration.java
@@ -1,14 +1,27 @@
 package com.cat.redis;
 
+import cn.hutool.core.util.StrUtil;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.BatchStrategies;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.Objects;
 
 /**
  * 可以优化为@EnableJsonRedis，或者直接叫JsonRedisTemplate得了
@@ -16,9 +29,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @EnableCaching
 @Configuration
+@EnableConfigurationProperties({CacheProperties.class})
 @AutoConfigureAfter(RedisAutoConfiguration.class)
 @ConditionalOnSingleCandidate(RedisConnectionFactory.class)
-public class RedisTemplateConfiguration extends CachingConfigurerSupport {
+public class RedisTemplateConfiguration /*extends CachingConfigurerSupport*/ {
     //<editor-fold defaultstate="collapsed" desc="delombok">
     @SuppressWarnings("all")
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(RedisTemplateConfiguration.class);
@@ -40,5 +54,43 @@ public class RedisTemplateConfiguration extends CachingConfigurerSupport {
         template.setHashValueSerializer(serializer);
         template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean
+    @Primary
+    public RedisCacheConfiguration redisCacheConfiguration(CacheProperties cacheProperties) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+        // 设置使用 : 单冒号，而不是双 :: 冒号，避免 Redis Desktop Manager 多余空格
+        // 详细可见 https://blog.csdn.net/chuixue24/article/details/103928965 博客
+        config = config.computePrefixWith(cacheName -> cacheName + StrUtil.COLON);
+        // 设置使用 JSON 序列化方式
+        config = config.serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
+
+        // 设置 CacheProperties.Redis 的属性
+        CacheProperties.Redis redisProperties = cacheProperties.getRedis();
+        if (redisProperties.getTimeToLive() != null) {
+            config = config.entryTtl(redisProperties.getTimeToLive());
+        }
+        if (redisProperties.getKeyPrefix() != null) {
+            config = config.prefixCacheNameWith(redisProperties.getKeyPrefix());
+        }
+        if (!redisProperties.isCacheNullValues()) {
+            config = config.disableCachingNullValues();
+        }
+        if (!redisProperties.isUseKeyPrefix()) {
+            config = config.disableKeyPrefix();
+        }
+        return config;
+    }
+    @Bean
+    public CacheManager timeoutRedisCacheManager(RedisTemplate redisTemplate, RedisCacheConfiguration redisCacheConfiguration) {
+        //log.info("=================>>>{}",redisTemplate instanceof JsonRedisTemplate);
+        // 创建 RedisCacheWriter 对象
+        RedisConnectionFactory connectionFactory = Objects.requireNonNull(redisTemplate.getConnectionFactory());
+        RedisCacheWriter cacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory,
+                BatchStrategies.scan(30));
+        // 创建 TenantRedisCacheManager 对象
+        return new TimeoutRedisCacheManager(cacheWriter, redisCacheConfiguration);
     }
 }
